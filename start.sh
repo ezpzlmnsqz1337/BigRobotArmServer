5899093be85a02edc5cbee05019ea198ef620e11
ezpzlmnsqz1337 <zden.braun@seznam.cz>
2022-08-27 22:08:42 +0200

    chore(sripts): fix start script so that it can be used as service

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 python3 -m http.server -d /home/pi/workspace/BigRobotArmServer/www &
-python3 /home/pi/workspace/BigRobotArmServer/websocket-server.py &
+python3 /home/pi/workspace/BigRobotArmServer/websocket-server.py
